9390b305d3d086bbf05248f60103f4730902dc11
Wai <Mincl5792@gmail.com>
2022-03-06 16:01:23 -0500

    removed trailing comma

--- src/utilities/assetManagement/assetUtililties.ts
+++ src/utilities/assetManagement/assetUtililties.ts
@@ -14,7 +14,7 @@ export default class AssetManagement {
      * @param {boolean} blurred | if the file you requested should be blurred. Will navigate to correct dir to get the image.
      * @returns {string} of the physical link that we have on disk.
      */
-    static async convertToPhysicalLink(type: AssetTypeStrings, link: string, blurred: boolean = false, ): Promise<string> {
+    static async convertToPhysicalLink(type: AssetTypeStrings, link: string, blurred: boolean = false): Promise<string> {
         blurred = (type == "characters" && blurred == true ? false : blurred) // Since character images can't be blurred (why would you even want that) we default it to false.
         return `../assets/${type}/${blurred ? 'blurred' : 'normal'}/${link}` // returen the file location.
     }
