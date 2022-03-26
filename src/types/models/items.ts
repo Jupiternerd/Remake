0e2a070d8048e0c190a1ec655d8a6b88f658f79d
Wai <Mincl5792@gmail.com>
2022-03-26 17:30:50 -0400

    items

--- src/types/models/items.ts
+++ src/types/models/items.ts
@@ -5,6 +5,7 @@ import { basic } from "../local/static";
 // author = shokkunn
 
 export interface Item extends basic {
+    giftable?: boolean,
     description: string;
-    stackable: boolean;
+    //stackable: boolean;
 }
