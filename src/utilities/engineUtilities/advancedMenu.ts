6783c0828b2f68ccd08658e6bf0cd1ae1956f572
Wai <Mincl5792@gmail.com>
2022-03-30 18:51:40 -0400

    stub

--- src/utilities/engineUtilities/advancedMenu.ts
+++ src/utilities/engineUtilities/advancedMenu.ts
@@ -1,12 +1,27 @@
-import NovelCore from "../../engines/novel/core";
+import { EventEmitter } from "events";
+import { CommandInteraction, MessageActionRow, MessageSelectOptionData } from "discord.js";
 
-export default class AdvanceMenu {
+
+export default class AdvanceMenu extends EventEmitter {
+
+    ARR: Array<Array<MessageSelectOptionData>>;
+    col: number;
+    row: number;
+    interaction: CommandInteraction;
 
 
-    novel: NovelCore
-
-    constructor(novel: NovelCore) {
-        this.novel = novel;
+    constructor(arrayOfArr: Array<Array<MessageSelectOptionData>>, interaction: CommandInteraction) {
+        super()
     }
 
+    public async action(): Promise<MessageActionRow[]> {
+        return;
+
+    }
+    public async start() {
+        this.interaction.editReply({
+            components: await this.action()
+        })
+        
+    }
 }
