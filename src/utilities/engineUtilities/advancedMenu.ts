3e02572dafd5332f057b806d70e9d6be5296c4d3
Wai <Mincl5792@gmail.com>
2022-03-30 14:38:45 -0400

    moving new system into its own class

---
+++ src/utilities/engineUtilities/advancedMenu.ts
@@ -0,0 +1,12 @@
+import NovelCore from "../../engines/novel/core";
+
+export default class AdvanceMenu {
+
+
+    novel: NovelCore
+
+    constructor(novel: NovelCore) {
+        this.novel = novel;
+    }
+
+}
