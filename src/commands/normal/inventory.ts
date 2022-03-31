97cd281c5694fbbe80670e9541ef05a895230d2a
Wai <Mincl5792@gmail.com>
2022-03-31 18:34:21 -0400

    inv command

--- src/commands/normal/inventory.ts
+++ src/commands/normal/inventory.ts
@@ -15,21 +15,12 @@ class Inventory extends Commands {
         "Shows your inventory",
         {
             coolDown: 5000
-        },
-        new SlashCommandBuilder().addSubcommand(subc => 
-            subc
-            .setName("test")
-            .setDescription("description for 1_test"))
-        )
+        })
     }
 
     // Executes the command.
     public async execute(bot: client, interaction: CommandInteraction): Promise<void> {
-        interaction.reply("dong");
-    }
-
-    public async test(bot: client, interaction: CommandInteraction): Promise<void> {
-        interaction.reply("Test!")
+        
     }
 }
 
